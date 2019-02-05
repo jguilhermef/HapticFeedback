9794bf4ebfe659bc98ea42569aa39d8584b599c3
jguilhermef <guilherme.limafreitas@hotmail.com>
2019-02-05 20:45:19 -0200

    Initial Commit

---
+++ HapticSampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapticSampleProject
+//
+//  Created by José Guilherme de Lima Freitas on 05/02/19.
+//  Copyright © 2019 JG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
